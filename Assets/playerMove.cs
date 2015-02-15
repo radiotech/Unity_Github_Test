553c0fd21aa3d1c5d91fb664370c27045d8754e1
A.J. <lavabirdaj@gmail.com>
2015-02-14 22:06:43 -0500

    quick test

---
+++ Assets/playerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject player;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+
+		GameObject go = Instantiate(Resources.Load("Trail"), new Vector3(Input.mousePosition.x/100,Input.mousePosition.y/100,0), new Quaternion(0,0,0,0)) as GameObject;
+
+		transform.Rotate ( new Vector3(.3f,.3f,.3f));
+
+		//move to mouse;
+	}
+}
---
+++ Assets/trail.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trail : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate ( new Vector3(1f,1f,1f));
+		transform.localScale -= new Vector3(0.01F, .01F, .01F);
+
+		if(transform.localScale.x < 0){
+			transform.Translate ( new Vector3(30f,.3f,.3f));
+		}
+
+	}
+}
